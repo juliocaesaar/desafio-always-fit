ce75b21e9e5e124618473282968e2293a84903b6
Julio Cesar <juliocesaar.ma@gmail.com>
2025-07-22 07:07:46 -0300

    feat: add stores

---
+++ src/stores/index.ts
@@ -0,0 +1,5 @@
+export { useAuthStore } from './authStore';
+export { useTrainingStore } from './trainingStore';
+export { useNutritionStore } from './nutritionStore';
+export { useProgressStore } from './progressStore';
+export { useStatisticsStore } from './statisticsStore'; 
